a4ea1e7d6e61e4f70106cd0e27cdb842f8ae3fb7
Dan Green <danngreen1@gmail.com>
2024-04-06 21:24:30 -0700

    Fix missing M_PI

--- plaits/dsp/oscillator/sine_oscillator.h
+++ plaits/dsp/oscillator/sine_oscillator.h
@@ -32,6 +32,7 @@
 #ifndef PLAITS_DSP_OSCILLATOR_SINE_OSCILLATOR_H_
 #define PLAITS_DSP_OSCILLATOR_SINE_OSCILLATOR_H_
 
+#include "stmlib/dsp/filter.h"
 #include "stmlib/dsp/dsp.h"
 #include "stmlib/dsp/parameter_interpolator.h"
 #include "stmlib/dsp/rsqrt.h"
